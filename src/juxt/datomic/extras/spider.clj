07cb31f8ff6feab8cfc7be0da781c5f2c6862cd6
Malcolm Sparks <malcolm@congreve.com>
2013-11-18 21:36:51 +0000

    Added first draft of spider, a function that can be used to build a map from paths that traverse the entity graph.

---
+++ src/juxt/datomic/extras/spider.clj
@@ -0,0 +1,31 @@
+;; Copyright © 2013, JUXT LTD. All Rights Reserved.
+;;
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this distribution.
+;;
+;; By using this software in any fashion, you are agreeing to be bound by the
+;; terms of this license.
+;;
+;; You must not remove this notice, or any other, from this software.
+
+(ns juxt.datomic.extras.spider)
+
+(defn spider
+  [row mapping]
+  (letfn [(tr [f]
+            (cond
+             (vector? f) (fn [x] (filter (apply comp (reverse (map tr f))) x))
+             (list? f) (partial map (first f))
+             :otherwise f)
+            )]
+    (reduce-kv
+     (fn [s k path]
+       (if-let
+           [val (reduce
+                 (fn [e f]
+                   (when e ((tr f) e)))
+                 row path)]
+         (assoc s k val)
+         s))
+     {} mapping)))
